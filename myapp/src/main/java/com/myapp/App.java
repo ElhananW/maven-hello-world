e0901123e81a1e04019b3b3a771190d9da263af2
ElhananW <matitia11@gmail.com>
2025-02-15 22:36:40 +0200

    Add a name to the Hello World message.

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,13 +1,21 @@
 package com.myapp;
 
 /**
- * Hello world!
+ * Hello world program that greets the user.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Default name 
+        String name = "Elhanan";
+        
+        // Update the name if an argument was provided
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        System.out.println("Hello World, " + name + "!");
     }
 }
